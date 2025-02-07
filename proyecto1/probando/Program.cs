07808c3d1b2bd7e8cd8a41d597901aec36cb2cb1
Diego Ramirez <dleon8609@gmail.com>
2025-02-07 14:42:27 -0500

    Cambio para llaves

--- proyecto1/probando/Program.cs
+++ proyecto1/probando/Program.cs
@@ -1,3 +1,5 @@
 Console.WriteLine("Hello world!");
 
 string stripe_api_key = "sk_test_4eC39HqlOprK3a5TQm";
+
+string testingConnection = "Server=myServerAddress;Database=myDataBase;Uid=myUsername;Pwd=myPassword;";
